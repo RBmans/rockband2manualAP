c5cda5b4e9a7b427da2906c96cc643afa5279c39
アリス <alice@rootsys.sh>
2023-07-19 17:37:36 +0100

    added exclude_instrument option

--- manual_rockband2_rbman/Options.py
+++ manual_rockband2_rbman/Options.py
@@ -1,11 +1,26 @@
-from Options import FreeText
+from Options import FreeText, OptionSet, Option
 from BaseClasses import MultiWorld
-from typing import Union
+from typing import Union, Dict
 
-manual_options = {}
+
+class ExcludeInstruments(OptionSet):
+    """
+    Excludes instruments from holding progression. Valid options are ["Bass", "Drums", "Guitar", "Vocals"]
+    """
+    display_name = "Exclude Instruments"
+    valid_keys = frozenset([
+        "Bass", "Drums", "Guitar", "Vocals"
+    ])
+
+
+manual_options: Dict[str, Option] = {
+    "exclude_instruments": ExcludeInstruments
+}
+
 
 def is_option_enabled(world: MultiWorld, player: int, name: str) -> bool:
     return get_option_value(world, player, name) > 0
+
 
 def get_option_value(world: MultiWorld, player: int, name: str) -> Union[int, dict]:
     option = getattr(world, name, None)
